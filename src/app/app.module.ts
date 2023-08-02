47db2c08d3babea4abdfc7c27f5a0eb98cb086c9
Charcrede <charcrede@gmail.com>
2023-08-02 11:02:19 +0100

    creation des composant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ChoiceComponent } from './choice/choice.component';
+import { TemplateComponent } from './template/template.component';
+import { FormulaireComponent } from './formulaire/formulaire.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ChoiceComponent,
+    TemplateComponent,
+    FormulaireComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
