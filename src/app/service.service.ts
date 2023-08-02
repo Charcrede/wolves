7260ecf5bcc2a351e4a996ba501cda81bb6147ca
Charcrede <charcrede@gmail.com>
2023-08-02 17:36:04 +0100

    liShower

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Categorie } from './categorie';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
-
+  finTab!: Categorie[];
+  index: number = 0;
   constructor() { }
+  liShower(tab: Array<Categorie>, tab2: Categorie[]){
+    setInterval(() => {
+        if (this.index < tab.length) {
+          console.log(this.index);
+          console.log(tab[this.index]);
+          this.index++;
+        
+        tab2.push(tab[this.index]);
+        console.log(tab2);
+        
+      }
+      }, 1000);
+    }
 }
