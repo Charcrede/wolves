bcefa732fdb560935cc3b43b86125469d5049f08
Charcrede <charcrede@gmail.com>
2023-08-03 14:07:09 +0100

    niveau 1 du chatbot

--- src/app/choice/choice.component.ts
+++ src/app/choice/choice.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Categorie } from '../categorie';
-import { CATEGORIES } from '../mock.categorie';
+import { VirtualTimeScheduler } from 'rxjs';
+import { Categorie, SousThemes, Themes } from '../categorie';
+import { CATEGORIES, THEMES } from '../mock.categorie';
 import { ServiceService } from '../service.service';
 
 @Component({
@@ -10,15 +11,33 @@ import { ServiceService } from '../service.service';
 })
 export class ChoiceComponent implements OnInit {
   index: number = 0;
+  selectedCategorie!: Categorie;
+  categorieThemes!: Themes;
   constructor(private service: ServiceService) { };
-  categories: Categorie[] = CATEGORIES
-  categorie: Array<Categorie> = [];
-  ngOnInit(): void {
+  categorie: Categorie[] = CATEGORIES
+  themes: Themes[] = THEMES;
+  onSelect(categorie: Categorie){
+    this.selectedCategorie = categorie;
+    this.categorie.forEach(el => {
+      el.state = false;
+    })
+     this.selectedCategorie.state = true;
+     this.selectedCategorie.choose = true;
+     this.categorieThemes = this.service.getCatTheme(this.selectedCategorie.id);
+     this.retard(this.categorieThemes.themes);
+    }
+    retard(array: SousThemes[]| Categorie[]){
+    let i = 0
     setInterval(()=>{
-      if (this.index < this.categories.length) {
-        this.categories[this.index].seen = true;
-        this.index++
+      if (i < array.length) {
+        array[i].state = true;
+        i++
       }
-    }, 1000)
+    }, 500);
+  }
+  ngOnInit(): void {
+    this.retard(this.categorie)
+  }
+  ngOnChanges(){
   }
 }
