8dfcf39ceb9d287bd3dd0ed7c773215959d5ec17
regisatl <regisattolou21@outlook.fr>
2023-08-04 14:44:50 +0100

    rien

--- src/app/animation.module.ts
+++ src/app/animation.module.ts
@@ -4,14 +4,5 @@ export const fadeInAnimation = trigger('fadeInAnimation', [
       transition(':enter', [
             style({ opacity: 0 }),
             animate('3s ease', style({ opacity: 1 })),
-            query(':enter', [
-                  style({ opacity: 0, transform: 'translateX(-500px)' }),
-                  stagger(100, [
-                        animate('300ms ease-out', style({ opacity: 1, transform: 'translateX(0)' }))
-                  ])
-            ], { optional: true }),
-            query(':leave', [
-                  animate('300ms', style({ opacity: 0 }))
-            ], { optional: true }),
       ])
 ])
