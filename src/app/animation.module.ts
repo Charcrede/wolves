08d6dcfd1fbdd41e419d113693cccf9eccabdedd
Charcrede <charcrede@gmail.com>
2023-08-04 15:51:53 +0100

    mlkmlkj

--- src/app/animation.module.ts
+++ src/app/animation.module.ts
@@ -1,17 +1,17 @@
 import { trigger, style, animate, transition, query, stagger } from '@angular/animations';
 
 export const fadeInAnimation = trigger('fadeInAnimation', [
-      transition(':enter', [
-            query(':enter', [
-                  style({ opacity: 0, transform: 'translateX(-500px)' }),
-                  stagger(100, [
-                        animate('300ms ease-out', style({ opacity: 1, transform: 'translateX(0)' }))
-                  ])
-            ], { optional: true }),
-            query(':leave', [
-                  animate('300ms', style({ opacity: 0 }))
-            ], { optional: true }),
-            style({ opacity: 0 }),
-            animate('3s ease', style({ opacity: 1 })),
-      ])
+      // transition(':enter', [
+      //       query(':enter', [
+      //             style({ opacity: 0, transform: 'translateX(-500px)' }),
+      //             stagger(100, [
+      //                   animate('300ms ease-out', style({ opacity: 1, transform: 'translateX(0)' }))
+      //             ])
+      //       ], { optional: true }),
+      //       query(':leave', [
+      //             animate('300ms', style({ opacity: 0 }))
+      //       ], { optional: true }),
+      //       style({ opacity: 0 }),
+      //       animate('3s ease', style({ opacity: 1 })),
+      // ])
 ])
