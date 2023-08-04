f9fce9fca5c569889e80896d77ef081088696d55
Possidius Capo <possidiusc@gmail.com>
2023-08-04 16:43:38 +0100

    toujours le meme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,7 @@ import { ReactiveFormsModule } from '@angular/forms';
             ChoiceComponent,
             TemplateComponent,
             FormulaireComponent,
-            MainComponent
+            MainComponent,
       ],
       imports: [
             BrowserModule,
