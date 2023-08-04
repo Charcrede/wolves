7f02d675cc9979b47277e738f55e44cc5a6c7b9a
Charcrede <charcrede@gmail.com>
2023-08-04 10:37:40 +0100

    csde

--- src/app/choice/choice.component.ts
+++ src/app/choice/choice.component.ts
@@ -45,36 +45,3 @@ export class ChoiceComponent implements OnInit {
     })
   }
 }
-
-function ngOnInit() {
-  throw new Error('Function not implemented.');
-}
-// export class ChoiceComponent implements OnInit {
-//       index: number = 0;
-//       selectedCategorie!: Categorie;
-//       categorieThemes!: Themes;
-//       constructor(private service: ServiceService) { };
-//       categorie: Categorie[] = CATEGORIES
-//       themes: Themes[] = THEMES;
-//       onSelect(categorie: Categorie) {
-//             this.selectedCategorie = categorie;
-//             this.categorie.forEach(el => {
-//                   el.state = false;
-//             })
-//             this.selectedCategorie.state = true;
-//             this.selectedCategorie.choose = true;
-//             this.categorieThemes = this.service.getCatTheme(this.selectedCategorie.id);
-//             this.retard(this.categorieThemes.themes);
-//       }
-//       retard(array: SousThemes[] | Categorie[]) {
-//             let i = 0
-//             setInterval(() => {
-//                   if (i < array.length) {
-//                         array[i].state = true;
-//                         i++
-//                   }
-//             }, 500);
-//       }
-//       ngOnChanges() {
-//       }
-// }
