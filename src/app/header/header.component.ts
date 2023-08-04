43ce858fa52bfd0b7cc266ee1ed32ea5548966b9
Charcrede <charcrede@gmail.com>
2023-08-04 18:01:20 +0100

    dsf

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { fadeInAnimation } from '../animation.module';
 import { ServiceService } from '../service.service';
 
@@ -9,6 +9,8 @@ import { ServiceService } from '../service.service';
   animations: [fadeInAnimation]
 })
 export class HeaderComponent implements OnInit {
+  start: boolean = true;
+  @Output() starte: EventEmitter<boolean> = new EventEmitter();
   newSentences: string = "";
   constructor(private service : ServiceService){}
   writter(sentences: string){
@@ -22,6 +24,9 @@ export class HeaderComponent implements OnInit {
         i++;
       }
     }, 70);
+  }
+  started(start: boolean){
+    this.starte.emit(start);
   }
   ngOnInit(): void {
     this.writter("Bienvenue sur notre site vitrine Chatbot ! Notre plateforme innovante vous permet de générer des sites Web sur mesure en quelques étapes simples. Grâce à notre Chatbot intuitif, vous serez guidé tout au long du processus de création. Choisissez parmi une variété de modèles personnalisables. Que vous soyez novice en développement Web ou expert, notre Chatbot convivial vous aidera à créer un site professionnel qui répond à vos besoins spécifiques. Essayez-le dès maintenant et découvrez la simplicité de la conception de sites Web avec notre Chatbot révolutionnaire")
