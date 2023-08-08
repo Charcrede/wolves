60c0c96f8fcf4a1ce8551778d794d49d1f3b89e1
Delanno Kotcho <delanktc24@gmail.com>
2023-08-08 17:26:09 +0100

    nouveau

--- src/app/choice/choice.component.ts
+++ src/app/choice/choice.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnChanges, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnChanges, OnInit, Output, Renderer2, ViewChild } from '@angular/core';
 // import { fadeInAnimation } from '../animation.module';
 import { Categorie, SousThemes, Themes } from '../categorie';
 import { CATEGORIES, THEMES } from '../mock.categorie';
@@ -20,84 +20,92 @@ import { count } from 'rxjs';
     styleUrls: ['./choice.component.css'],
     // animations: [fadeInAnimation]
 })
-export class ChoiceComponent implements OnInit, OnChanges {
-    constructor(private service: ServiceService) {
-        let choice = document.getElementById('choice')
-        console.log(choice);
-
-    };
-    @Output() form: EventEmitter<boolean> = new EventEmitter();
-    @Output() templ: EventEmitter<boolean> = new EventEmitter();
-    index: number = 0;
-    temp: boolean = true;
-    bonj: boolean = false;
-    cate: boolean = false;
-    seeTheme: boolean = false;
-    seeTitle: boolean = false;
-    template: boolean = false;
-    colored: boolean = false;
-    color: boolean = false;
-    seeColor: boolean = false;
-    fiText: boolean = false;
-    confirm: number = 0;
-    selectedCategorie!: Categorie;
-    categorieThemes!: Themes;
-    categories: Categorie[] = CATEGORIES
-    themes: Themes[] = THEMES;
-    selectedTheme: string[] = [];
-    inputValue: string = "";
-    time: any = "";
-    minute: any = "";
+export class ChoiceComponent implements OnInit {
+    constructor(
+        private service: ServiceService,
+        private renderer: Renderer2
+        ) {};
+    @ViewChild('choice') choice !: ElementRef;
+    ngAfterViewInit():void{
+        this.scrollToBottom();
+      }
+      scrollToBottom(){
+        const container = this.choice.nativeElement;
+        container.scrollTop = container.scrollHeight;
+        console.log(container);
+        
+      }
+      @Output() form : EventEmitter<boolean> = new EventEmitter();
+      @Output() templ : EventEmitter<boolean> = new EventEmitter();
+      index: number = 0;
+      colored: boolean =false;
+      seeColor: boolean = false;
+      temp: boolean = true;
+      bonj: boolean = false;
+      color: boolean = false;
+      cate: boolean = false;
+      cate2: boolean = false;
+      seeTheme : boolean = false;
+      seeTitle : boolean = false;
+      template : boolean = false;
+      fiText : boolean = false;
+      confirm: number = 0;
+      selectedCategorie!: Categorie;
+      categorieThemes!: Themes;
+      categories: Categorie[] = CATEGORIES
+      themes: Themes[] = THEMES;
+      selectedTheme: string[] = [];
+      inputValue: string = "";
+      time: any = "";
+      minute:any ="";
 
     heure: any = this.time + this.minute
 
-    onSelect(categorie: Categorie) {
-        this.selectedCategorie = categorie;
-        // this.categories.forEach(el => {
-        //       el.state = false;
-        // })
-        this.selectedCategorie.state = true;
-        this.selectedCategorie.choose = true;
-        this.categorieThemes = this.service.getCatTheme(this.selectedCategorie.id);
-        this.retard(this.categorieThemes.themes);
-        setTimeout(() => {
-            this.seeTheme = true;
-            // this.retard(this.categories)
-        }, 1000);
-    }
-    retard(array: SousThemes[] | Categorie[]) {
-        let i = 0
-        setInterval(() => {
-            if (i < array.length) {
-                array[i].state = true;
-                console.log(array[i]);
-                i++
+      onSelect(categorie: Categorie) {
+            this.selectedCategorie = categorie;
+            // this.categories.forEach(el => {
+                  //       el.state = false;
+                  // })
+                  this.selectedCategorie.state = true;
+                  this.selectedCategorie.choose = true;
+            this.categorieThemes = this.service.getCatTheme(this.selectedCategorie.id);
+            this.retard(this.categorieThemes.themes);
+            setTimeout(() => {
+                  this.seeTheme = true;
+                  // this.retard(this.categories)
+            }, 1000);
+      }
+      retard(array: SousThemes[] | Categorie[]) {
+            let i = 0
+            setInterval(() => {
+                  if (i < array.length) {
+                        array[i].state = true;
+                        i++
+                  }
+            }, 100);
+      }
+      ngOnInit(): void {
+            setTimeout(() => {
+                  this.bonj = true;
+            }, 1000);
+            setTimeout(() => {
+                  this.cate = true;
+                  this.retard(this.categories)
+            }, 2000);
+            setTimeout(() => {
+                  this.cate2 = true;
+                  this.retard(this.categories)
+            }, 3000);
+      }
+      startForm(bool : boolean){
+            this.templ.emit(bool);
+      }
+      themer(){
+            this.selectedTheme.push(this.inputValue);
+            this.confirm++;
+            if (this.confirm >= 3 && this.fiText) {
+                  this.template = true;
             }
-        }, 100);
-    }
-    ngOnInit(): void {
-        setTimeout(() => {
-            this.bonj = true;
-        }, 1000);
-        setTimeout(() => {
-            this.cate = true;
-            this.retard(this.categories)
-        }, 2000);
-    }
-    startForm(bool: boolean) {
-        this.form.emit(bool);
-    }
-    startTemp(bool: boolean) {
-        this.templ.emit(bool);
-    }
-    themer() {
-        this.selectedTheme.push(this.inputValue);
-        this.confirm++;
-        console.log(this.confirm);
-
-        if (this.confirm === 3 && this.fiText) {
-            this.template = true;
-        }
         if (this.confirm === 2) {
             setTimeout(() => {
                 this.fiText = true;
@@ -124,12 +132,6 @@ export class ChoiceComponent implements OnInit, OnChanges {
     getCarousels(): Carousel[] {
         return CAROUSELS;
     }
-
-    ngOnChanges(): void {
-        console.log('change marche');
-
-    }
-
     times() {
         let time = new Date()
         let hours = 0;
